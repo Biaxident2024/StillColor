8ef3e82364e61431e1c930bb0f72ac8ca653d258
GMMDMDIDEMS <jan.kautenburger@gmail.com>
2024-09-12 12:28:50 +0200

    Add backward compatibility for macOS 12.0+

--- Stillcolor/StillcolorApp.swift
+++ Stillcolor/StillcolorApp.swift
@@ -3,6 +3,7 @@
 //  Stillcolor
 //
 //  Created by Abdullah Arif on 25/02/2024.
+//  Modified by GMMDMDIDEMS on 12/09/2024.
 //
 
 import SwiftUI
@@ -14,49 +15,89 @@ struct StillcolorApp: App {
     @AppStorage("disableUniformity2D") var disableUniformity2D: Bool = false
     
     var detector = ScreenDetector();
+    var statusBarItem: NSStatusItem?
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
-        detector.addObervers()
+        detector.addObservers()
         Stillcolor.enableDisableDithering(disableDithering)
         Stillcolor.enableDisableUniformity2D(disableUniformity2D)
     }
     
     var body: some Scene {
-        MenuBarExtra(
-            "Stillcolor",
-            systemImage: "\(disableDithering  ? "livephoto.slash" : "livephoto")"
-        ) {
-            Toggle("Disable Dithering", isOn: .init(
-                get: { disableDithering },
-                set: {
-                    disableDithering = $0
-                    Stillcolor.enableDisableDithering(disableDithering)
-                }
-            ))
-            
-            Toggle("Disable uniformity2D", isOn: .init(
-                get: { disableUniformity2D },
-                set: {
-                    disableUniformity2D = $0
-                    Stillcolor.enableDisableUniformity2D(disableUniformity2D)
-                }
-            ))
-            
-            Label {
-                Text("(Experimental) Stop built-in display from\nusing lower brightness levels around the edges")
-                    .font(.caption)
-                    .fontWeight(.thin)
-                    .foregroundColor(.secondary)
-                    .multilineTextAlignment(.leading)
-            } icon: {
-            }
-            
-            Divider()
-            LaunchAtLogin.Toggle()
-            Divider()
-            Button("Quit Stillcolor") {
-                NSApplication.shared.terminate(nil)
-            }.keyboardShortcut("q")
+        Settings{
+            EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @AppStorage("disableDithering") var disableDithering: Bool = true
+
+    var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: disableDithering ? "livephoto.slash" : "livephoto", accessibilityDescription: nil)
+        }
+
+        constructMenu()
+    }
+
+    private func constructMenu() {
+        let menu = NSMenu()
+
+        let menuItem = NSMenuItem(title: "", action: nil, keyEquivalent: "")
+        let hostingView = NSHostingController(rootView: MenuView())
+        hostingView.view.frame = NSRect(x: 0, y: 0, width: 300, height: 125)
+        menuItem.view = hostingView.view
+
+        menu.addItem(menuItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Stillcolor", action: #selector(quit), keyEquivalent: "q"))
+
+        statusItem?.menu = menu
+    }
+
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
+    }
+}
+
+struct MenuView: View {
+    @AppStorage("disableDithering") var disableDithering: Bool = true
+    @AppStorage("disableUniformity2D") var disableUniformity2D: Bool = false
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Toggle(isOn: $disableDithering) {
+                Text("Disable Dithering")
+            }
+            .onChange(of: disableDithering) { newValue in
+                Stillcolor.enableDisableDithering(newValue)
+            }
+
+            Toggle(isOn: $disableUniformity2D) {
+                Text("Disable uniformity2D")
+            }
+            .onChange(of: disableUniformity2D) { newValue in
+                Stillcolor.enableDisableUniformity2D(newValue)
+            }
+
+            Text("(Experimental) Stop built-in display from using lower brightness levels around the edges")
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .fixedSize(horizontal: false, vertical: true)
+                .padding(.leading, 20)
+
+            Divider()
+
+            LaunchAtLogin.Toggle()
+        }
+        .padding()
+        .frame(width: 300)
     }
 }
