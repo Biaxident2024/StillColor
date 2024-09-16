9d5da20fe49598db4121b54dca8fac3edae9a0ba
GMMDMDIDEMS <jan.kautenburger@gmail.com>
2024-09-16 12:08:06 +0200

    Add singleton to manage app-state, add delegate to update menu bar icon

--- Stillcolor/StillcolorApp.swift
+++ Stillcolor/StillcolorApp.swift
@@ -9,92 +9,121 @@
 import SwiftUI
 import LaunchAtLogin
 
+// Delegate protocol to update menu bar icon
+// https://developer.apple.com/documentation/swift/using-delegates-to-customize-object-behavior
+protocol AppStateDelegate: AnyObject {
+    func updateStatusItemImage()
+}
+
+// Singleton for managing application-wide state
+// https://developer.apple.com/documentation/swift/managing-a-shared-resource-using-a-singleton
+class AppState: ObservableObject {
+    static let shared = AppState()
+    
+    @Published var disableDithering: Bool {
+        didSet {
+            UserDefaults.standard.set(disableDithering, forKey: "disableDithering")
+            Stillcolor.enableDisableDithering(disableDithering)
+            // Update menu bar icon
+            delegate?.updateStatusItemImage()
+        }
+    }
+    
+    @Published var disableUniformity2D: Bool {
+        didSet {
+            UserDefaults.standard.set(disableUniformity2D, forKey: "disableUniformity2D")
+            Stillcolor.enableDisableUniformity2D(disableUniformity2D)
+        }
+    }
+    
+    weak var delegate: AppStateDelegate?
+    
+    private init() {
+        self.disableDithering = UserDefaults.standard.bool(forKey: "disableDithering")
+        self.disableUniformity2D = UserDefaults.standard.bool(forKey: "disableUniformity2D")
+    }
+}
+
 @main
 struct StillcolorApp: App {
-    @AppStorage("disableDithering") var disableDithering: Bool = true
-    @AppStorage("disableUniformity2D") var disableUniformity2D: Bool = false
-    
-    var detector = ScreenDetector();
-    var statusBarItem: NSStatusItem?
+    var detector = ScreenDetector()
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
         detector.addObservers()
-        Stillcolor.enableDisableDithering(disableDithering)
-        Stillcolor.enableDisableUniformity2D(disableUniformity2D)
+        Stillcolor.enableDisableDithering(AppState.shared.disableDithering)
+        Stillcolor.enableDisableUniformity2D(AppState.shared.disableUniformity2D)
+        AppState.shared.delegate = appDelegate
     }
     
     var body: some Scene {
-        Settings{
+        Settings {
             EmptyView()
         }
     }
 }
 
-class AppDelegate: NSObject, NSApplicationDelegate {
-    @AppStorage("disableDithering") var disableDithering: Bool = true
-
+// https://developer.apple.com/documentation/swift/using-delegates-to-customize-object-behavior
+class AppDelegate: NSObject, NSApplicationDelegate, AppStateDelegate {
     var statusItem: NSStatusItem?
-
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
-        if let button = statusItem?.button {
-            button.image = NSImage(systemSymbolName: disableDithering ? "livephoto.slash" : "livephoto", accessibilityDescription: nil)
-        }
-
+        updateStatusItemImage()
         constructMenu()
     }
-
+    
+    func updateStatusItemImage() {
+        if let button = statusItem?.button {
+            let imageName = AppState.shared.disableDithering ? "livephoto.slash" : "livephoto"
+            button.image = NSImage(systemSymbolName: imageName, accessibilityDescription: nil)
+        }
+    }
+    
     private func constructMenu() {
         let menu = NSMenu()
-
-        let menuItem = NSMenuItem(title: "", action: nil, keyEquivalent: "")
+        
+        let menuItem = NSMenuItem()
         let hostingView = NSHostingController(rootView: MenuView())
-        hostingView.view.frame = NSRect(x: 0, y: 0, width: 300, height: 125)
+        // Change width to 295 to improve item alignment
+        hostingView.view.frame = NSRect(x: 0, y: 0, width: 295, height: 110)
         menuItem.view = hostingView.view
-
+        
         menu.addItem(menuItem)
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Stillcolor", action: #selector(quit), keyEquivalent: "q"))
-
+        
         statusItem?.menu = menu
     }
-
+    
     @objc func quit() {
         NSApplication.shared.terminate(self)
     }
 }
 
 struct MenuView: View {
-    @AppStorage("disableDithering") var disableDithering: Bool = true
-    @AppStorage("disableUniformity2D") var disableUniformity2D: Bool = false
+    @ObservedObject var appState = AppState.shared
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Toggle(isOn: $disableDithering) {
+        VStack(alignment: .leading, spacing: 8) {
+            Toggle(isOn: $appState.disableDithering) {
                 Text("Disable Dithering")
             }
-            .onChange(of: disableDithering) { newValue in
-                Stillcolor.enableDisableDithering(newValue)
+            
+            Toggle(isOn: $appState.disableUniformity2D) {
+                Text("Disable Uniformity2D")
             }
-
-            Toggle(isOn: $disableUniformity2D) {
-                Text("Disable uniformity2D")
-            }
-            .onChange(of: disableUniformity2D) { newValue in
-                Stillcolor.enableDisableUniformity2D(newValue)
-            }
-
+            
             Text("(Experimental) Stop built-in display from using lower brightness levels around the edges")
                 .font(.caption)
                 .foregroundColor(.secondary)
                 .fixedSize(horizontal: false, vertical: true)
+                .padding(.top, -6) // reduce vertical space
                 .padding(.leading, 20)
-
+            
             Divider()
-
+            
             LaunchAtLogin.Toggle()
         }
         .padding()
